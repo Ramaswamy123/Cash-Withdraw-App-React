918fecddb56964fab5694f95dcbcf16362515a87
Ramaswamy123 <ramisettiramaswamy555@gmail.com>
2022-09-24 07:03:46 +0000

    create withdraw appliaction using react js

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,48 @@
+import {Component} from 'react'
+import DenominationItem from '../DenominationItem'
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {balance: 2000}
+
+  updateBalance = value => {
+    this.setState(prevState => ({balance: prevState.balance - value}))
+  }
+
+  render() {
+    const {balance} = this.state
+    const {denominationsList} = this.props
+    const name = 'Sarah Williams'
+    const initial = name.slice(0, 1)
+    return (
+      <div className="bg-container">
+        <div className="withdraw-container">
+          <div className="profile-container">
+            <div className="initial">{initial}</div>
+            <p className="name">{name}</p>
+          </div>
+          <div className="balance-container">
+            <p className="main-balance">Your Balance</p>
+            <p className="balance">
+              {balance}
+              <br />
+              <span className="currency">In Rupees</span>
+            </p>
+          </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachItem => (
+              <DenominationItem
+                denominationDetails={eachItem}
+                key={eachItem.id}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default CashWithdrawal
---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationDetails, updateBalance} = props
+  const {value} = denominationDetails
+
+  onClickDenomination = () => {
+    updateBalance(value)
+  }
+  return (
+    <li className="denomination-item">
+      <button
+        className="denomination-button"
+        type="button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+export default DenominationItem
